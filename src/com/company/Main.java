9ed6c85139f5ba5f47b12a2dc4229292cd3f4e44
Azatalapov <alapov.azater@mail.ru>
2020-10-27 15:35:12 +0300

    Task1_6. Все задания

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,52 @@ public class Main {
         Scanner in = new Scanner(System.in);
         String k = in.nextLine();
         String[] massivS = k.split(" ");
+        double a = Double.valueOf(massivS[0]);
+        double b = Double.valueOf(massivS[1]);
+        if (a <= 0 && b <= 0)
+        {
+            System.out.printf("no such x");
+        }
+       else {
+            if ((a > 0 && b >= 0) || (a == 0 && b > 0)) {
+                System.out.printf("any x");
+            }
+            else {
+                if (a > 0 && b < 0)
+                {
+                    double x1 = b/a;
+                    double x2 = x1*(-1);
+                    System.out.printf("x<%f or x>%f",x1,x2);
+                }
+                else
+                {
+                    double x1 = b/a;
+                    double x2 = x1*(-1);
+                    System.out.printf("%f<x<%f",x1,x2);
+                }
+            }
+        }
+
+
+        /*int s = in.nextInt();
+        int p = s%10;
+        if(p==1 && s != 11)
+        {
+            System.out.println(s + " TOPT");
+        }
+        else
+        {
+            if (p >= 2 && p <= 4 && !(s>=12 && s <=14))
+            {
+                System.out.println(s + " TOPTA");
+            }
+            else
+            {
+                System.out.println(s + " TOPTOB");
+            }
+        }
+        /*String k = in.nextLine();
+        String[] massivS = k.split(" ");
         int u = Integer.valueOf(massivS[0]);
         int i = Integer.valueOf(massivS[1]);
         int r = Integer.valueOf(massivS[2]);
